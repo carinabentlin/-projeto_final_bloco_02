02e941e523b96d673709ac5bb5433e3fdfe389b8
carina <carinabentlin@gmail.com>
2025-10-27 09:42:14 -0300

    :tada: Criação do projeto FarmaciaOn

---
+++ src/main/java/com/generation/blogpessoal/FarmaciaOnApplication.java
@@ -0,0 +1,13 @@
+package com.generation.blogpessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaOnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/blogpessoal/FarmaciaOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.blogpessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
